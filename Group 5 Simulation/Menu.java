4c477e15e0312166ea894d9ecdb3d7225401cbac
marin-yan <marinxyan@gmail.com>
2026-04-20 12:58:01 -0400

    added images to the menu

--- Group 5 Simulation/Menu.java
+++ Group 5 Simulation/Menu.java
@@ -15,6 +15,11 @@ public class Menu extends Actor
     
     private String[] menuOptions = {"Cookie", "Stack of Cookies"};
     
+    private GreenfootImage[] menuImages = {
+        new GreenfootImage("Menu/cookie.png"),
+        new GreenfootImage("Menu/cookiestack.webp")
+    };
+    
     public void act()
     {
         // Add your action code here.
