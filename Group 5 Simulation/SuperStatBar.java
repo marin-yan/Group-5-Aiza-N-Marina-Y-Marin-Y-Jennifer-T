8c104bb8e0eb058efdccb77ef7bf99ab823dca9f
350375325-web <350375325@gapps.yrdsb.ca>
2026-04-20 12:30:05 -0400

    Added SuperStatBar

--- Group 5 Simulation/SuperStatBar.java
+++ Group 5 Simulation/SuperStatBar.java
@@ -1,15 +1,15 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Staff here.
+ * Write a description of class SuperStatBar here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Staff extends SuperSmoothMover
+public class SuperStatBar extends SuperSmoothMover
 {
     /**
-     * Act - do whatever the Staff wants to do. This method is called whenever
+     * Act - do whatever the SuperStatBar wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act()
