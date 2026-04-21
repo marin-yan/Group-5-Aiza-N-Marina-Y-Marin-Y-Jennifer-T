f0ad1b2a15feaf11180ab353929c2748358c8048
marin-yan <marinxyan@gmail.com>
2026-04-21 13:05:29 -0400

    added a order() method to the customer

--- Group 5 Simulation/Customer.java
+++ Group 5 Simulation/Customer.java
@@ -6,14 +6,35 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Customer extends Actor
-{
+public abstract class Customer extends SuperSmoothMover {
     /**
      * Act - do whatever the Customer wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    public void act()
-    {
-        // Add your action code here.
+    private Menu menu;
+    private boolean hasOrdered = false;
+    private int orderedFoodIndex;
+    
+    public void act() {
+        if (!hasOrdered)
+        {
+            orderFood();
+        }
+    }
+    
+    public Customer(Menu menu) {
+        this.menu = menu;
+        setImage("customer.png");
+    }
+    
+    public void orderFood() {
+        orderedFoodIndex = Greenfoot.getRandomNumber(menu.getMenuSize());
+        hasOrdered = true;
+        displayOrder(orderedFoodIndex);
+    }
+    
+    
+    public void displayOrder(int index) {
+        GreenfootImage orderedImage = menu.getMenuImages() [orderedFoodIndex];
     }
 }
--- Group 5 Simulation/Menu.java
+++ Group 5 Simulation/Menu.java
@@ -8,11 +8,6 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Menu extends Actor
 {
-    /**
-     * Act - do whatever the Menu wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
-    
     private String[] menuOptions = {"Cookie", "Stack of Cookies"};
     
     private GreenfootImage[] menuImages = {
@@ -25,11 +20,22 @@ public class Menu extends Actor
         // Add your action code here.
     }
     
-    public String [] getMenuOptions() {
+    public String[] getMenuOptions()
+    {
         return menuOptions;
     }
-    
+
     public GreenfootImage[] getMenuImages() {
         return menuImages;
+    }
+    
+    public String getMenuOption(int index)
+    {
+        return menuOptions[index];
+    }
+    
+    public int getMenuSize()
+    {
+        return menuOptions.length;
     }
 }
