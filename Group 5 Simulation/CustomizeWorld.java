15b2f25ed34c0d08313183eef4ee8ff2b2c1f74a
350375325-web <350375325@gapps.yrdsb.ca>
2026-04-24 12:27:51 -0400

    Added a world for customization

---
+++ Group 5 Simulation/CustomizeWorld.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class CustomizeWorld here.
+ * 
+ * @author (Aiza) 
+ * @version (a version number or a date)
+ */
+public class CustomizeWorld extends World
+{
+
+    /**
+     * Constructor for objects of class CustomizeWorld.
+     * 
+     */
+    public CustomizeWorld()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+    }
+}
