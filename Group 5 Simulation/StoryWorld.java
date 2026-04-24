22cc35da80c367a0742c352ea51cd45cdad23385
349070227-hub <349070227@gapps.yrdsb.ca>
2026-04-24 12:59:22 -0400

    added backgrounds for customization and story world

--- Group 5 Simulation/StoryWorld.java
+++ Group 5 Simulation/StoryWorld.java
@@ -15,7 +15,9 @@ public class StoryWorld extends World
      */
     public StoryWorld()
     {    
-        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        // Create a new world with 1200x800 cells with a cell size of 1x1 pixels.
+        super(1200, 800, 1); 
+        setBackground(new GreenfootImage("images/Background/customization background.png"));
+        Greenfoot.setWorld(this);
     }
 }
