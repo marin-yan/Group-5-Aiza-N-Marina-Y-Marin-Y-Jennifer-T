13f988bb6365f43478014e45a334909bf0494f1f
JenniferTuWu <jenniferydt@gmail.com>
2026-04-24 21:18:40 -0400

    added customizeWorld template

--- Group 5 Simulation/CustomizeWorld.java
+++ Group 5 Simulation/CustomizeWorld.java
@@ -4,12 +4,15 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * Write a description of class CustomizeWorld here.
  * 
  * @author (Aiza)(Jennifer)
+ * Credit: Pinterest "Bg for edits" - zeee
+ *         Pinterest -JUNGLORS
  * @version (a version number or a date)
  */
 public class CustomizeWorld extends World
 {
-    private GreenfootImage background;
-    
+    private GreenfootImage background; // brown
+    private GreenfootImage bg; // white
+    private GreenfootImage bg1;
     /**
      * Constructor for objects of class CustomizeWorld.
      * 
@@ -19,6 +22,11 @@ public class CustomizeWorld extends World
         // Create a new world with 1200x800 cells with a cell size of 1x1 pixels.
         super(1200, 800, 1);
         background = new GreenfootImage("images/Background/customizeWorldbg.JPG");
+        bg = new GreenfootImage("images/Background/characterBg.png");
+        bg1 = new GreenfootImage("images/Background/characterBg.png");
+        bg1.mirrorHorizontally();
+        background.drawImage(bg, -25, -100);
+        background.drawImage(bg1, 575, -100);
         setBackground(background);
     }
 }
