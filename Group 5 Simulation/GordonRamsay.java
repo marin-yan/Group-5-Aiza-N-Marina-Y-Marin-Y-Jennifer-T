7943e378f990572db6a18fd33530a8238c3e6e02
349070227-hub <349070227@gapps.yrdsb.ca>
2026-04-27 01:28:49 -0400

    added image of owner and GordonRamsay to storyworld

---
+++ Group 5 Simulation/GordonRamsay.java
@@ -0,0 +1,26 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class GordonRamsey here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class GordonRamsay extends Actor
+{
+    private GreenfootImage gordon;
+
+    public GordonRamsay() {
+        gordon = new GreenfootImage("images/Story/gordon ramsay.png");
+        setImage(gordon);
+    }
+    
+    /**
+     * Act - do whatever the GordonRamsey wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        
+    }
+}
--- Group 5 Simulation/StoryWorld.java
+++ Group 5 Simulation/StoryWorld.java
@@ -8,7 +8,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class StoryWorld extends World
 {
-    private int timer = 0;
+    private int timer = 300;
     
     /**
      * Constructor for objects of class StoryWorld.
@@ -18,14 +18,21 @@ public class StoryWorld extends World
     {    
         // Create a new world with 1200x800 cells with a cell size of 1x1 pixels.
         super(1200, 800, 1); 
-        setBackground(new GreenfootImage("images/Background/story background.png"));
+        setBackground(new GreenfootImage("images/Background/StoryWorld.png"));
         Greenfoot.setWorld(this);
+        
+        addObject (new GordonRamsay(), 350, 400);
+        
+        Owner owner = new Owner(0, false);
+        addObject (owner, 850, 400);
+        owner.getImage().scale(370, 370); // change scale of image specifically for StoryWorld
+
+        
     }
     
     public void act() {
-        timer++;
-        
-        if (timer > 300) {
+        timer--;
+        if (timer < 0) {
             Greenfoot.setWorld(new CustomizeWorld());
         }
     }
