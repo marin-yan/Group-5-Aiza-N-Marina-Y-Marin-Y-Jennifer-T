6fd668e6354154a6cbc2bb9478de73584344984a
350375325-web <350375325@gapps.yrdsb.ca>
2026-04-27 21:10:13 -0400

    uncommitted files

---
+++ Group 5 Simulation/LevelStatBar.java
@@ -0,0 +1,50 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class LevelStatBar here.
+ * 
+ * @author (Aiza) 
+ * @version (a version number or a date)
+ */
+public class LevelStatBar extends SuperStatBar
+{   
+    //stat bar for levels variables
+    private int totalCoins = 0;
+    private int coinsPerLevel = 50;
+    private int level = 1;
+    private int maxLevel = 3;
+
+    public LevelStatBar()
+    {
+        super(50, 0, null, 200, 20, 0, Color.GREEN, Color.GRAY, false, Color.BLACK, 2);
+    }
+
+    public void addCoins(int amount)
+    {
+        totalCoins += amount;
+
+        // Calculate level (max 3?)
+        level = Math.min((totalCoins / coinsPerLevel) + 1, maxLevel);
+
+        // Coins within level (0–49)
+        int currentLevelCoins = totalCoins % coinsPerLevel;
+
+        // Update bar fill
+        update(currentLevelCoins);
+    }
+
+    public int getLevel()
+    {
+        return level;
+    }
+
+    public int getTotalCoins()
+    {
+        return totalCoins;
+    }
+    
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
