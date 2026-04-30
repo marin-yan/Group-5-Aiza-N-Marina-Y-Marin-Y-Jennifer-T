8b4d3f979936074e7c9ca20b1b62174dc7a5372e
JenniferTuWu <jenniferydt@gmail.com>
2026-04-30 09:20:48 -0400

    some adjustments

---
+++ Group 5 Simulation/AdjustmentButton.java
@@ -0,0 +1,53 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class AdjustmentButton here.
+ * 
+ * @author (Marin) (Jennifer - adjust the button(transparent bg))
+ * @version (a version number or a date)
+ */
+public class AdjustmentButton extends Button
+{
+    //player is either 1 or 2
+    private int value;
+    //direction is either 1 or -1
+    private int direction;
+    //type is either 1 (waiter count) or 2 (waiter speed)
+    private int type;
+    
+    public AdjustmentButton(int value, int direction, int type) {
+        super(getArrowText(direction), 40);
+        
+        this.value = value;
+        this.direction = direction;
+        this.type = type;
+        
+        GreenfootImage image = new GreenfootImage(40, 48);
+        image.setColor(new Color(0, 0, 0, 0)); // transparent background
+        image.fill();
+        
+        image.setColor(Color.BLACK); // keep + and - visible
+        image.setFont(new Font("Comic Sans MS", true, false, 24));
+        drawCenteredText(image, getArrowText(direction), 32);
+        
+        setImage(image);
+    }
+    
+    private static String getArrowText(int direction)
+    {
+        if (direction == -1)
+            return "-";
+        else
+            return "+";
+    }
+    
+    // Change display images in CustomizeWorld
+    public void onClick(){
+        CustomizeWorld world = (CustomizeWorld)getWorld();
+        if(type == 1){
+            world.changeWaiterCount(value, direction);
+        }else if(type == 2){
+            world.changeWaiterSpeed(value, direction);
+        }
+    }
+}
